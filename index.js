eb4c46c6c6a78d744d7807ab9d13dce012fa6c35
ArdorEast <lenovolenovs820@gmail.com>
2022-01-24 13:42:51 +0200

    feat: add adaptation and burger

--- index.js
+++ index.js
@@ -43,3 +43,49 @@ console.log(
 Все пункты выполнены полностью!');
 }
 assessment();
+
+// Бургер-меню
+// Взяли в константу иконку бургера
+const burger = document.querySelector('.header-burger');
+// Взяли в константу класс нашей навигации
+const openMenu = document.querySelector('.header__nav');
+const navLinks = document.querySelectorAll('.header__list-link');
+// Если класс иконки существует, то делаем то, что в фигурных скобках 
+if (burger) {
+// А именно.
+// Пишем функции, которая выполнится при клике на бургер
+    burger.addEventListener('click', function () {
+// Функция добавит класс .open для бургера и для навигации
+// В стилях css будем описывать его свойства, добавляя к класса
+// навигации и бургера без пробела (header__burger.open и header__nav.open)
+        burger.classList.toggle('open');
+        openMenu.classList.toggle('open');
+// Также добавляем класс .lock для body, чтобы запретить ему скролл
+// при открытом меню
+        document.body.classList.toggle('lock');
+    });
+    
+};
+// Теперь, чтобы меню закрывалось при клике на ссылку
+// Делаем проверку
+// Если у иконки бургера активен статус .open, то удаляем
+// при клике на ссылку этот класс .open и удаляем запрет
+// скролла для body
+
+
+function closeMenu() {
+    burger.classList.remove('open');
+    openMenu.classList.remove('open');
+    document.body.classList.remove('lock');
+};
+// openMenu.addEventListener('click', closeMenu);
+
+
+
+// if (burger.classList.contains('open')) {
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', closeMenu);
+    }    
+// }
+
+
